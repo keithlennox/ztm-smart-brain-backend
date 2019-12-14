157f9c0a0cdc7dde6b3df32f37598b07949f93db
klennox <klennox@tvo.org>
2019-12-13 20:33:39 -0500

    added signin route

--- server.js
+++ server.js
@@ -5,12 +5,41 @@ const app = express();
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
+const database = {
+  users: [
+    {
+      id: '123',
+      name: 'John',
+      email: 'john@gmail.com',
+      password: 'cookies',
+      entires: 0,
+      joined: new Date()
+    },
+    {
+      id: '124',
+      name: 'Sally',
+      email: 'sally@gmail.com',
+      password: 'bananas',
+      entires: 0,
+      joined: new Date()
+    }
+  ]
+}
+
+// / --> res = this is working
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-// / --> res = this is working
 // /signin --> POST = success/fail
+app.post('/signin', (req, res) => {
+  if(req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
+    res.json('success');
+  } else {
+    res.status(400).json('error logging in');
+  }
+})
+
 // /register --> POST = user
 // /profile/:userid --> GET = user
 // /image --> PUT = user
