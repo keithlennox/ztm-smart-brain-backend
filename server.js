b130de22bc5fbcff21c96cf73a8d9ea685a2ce5e
klennox <klennox@tvo.org>
2021-01-13 09:09:27 -0500

    updated db connection credentials

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host: 'localhost',
-    user: 'xxxxxxxx',
-    password: 'xxxxxxxxx',
-    database: 'smartbrain'
+    host: 'process.env.DATABASE_URL',
+    ssl: true
   }
 });
 
